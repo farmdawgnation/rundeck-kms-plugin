465d00f43bd1aca7df9eee3df53449dfdb791995
Matt Farmer <matt@frmr.me>
2021-02-07 13:30:02 -0500

    Iterating on the plugin implemenation

--- lib/src/main/java/me/frmr/rundeck/KmsConverterPlugin.java
+++ lib/src/main/java/me/frmr/rundeck/KmsConverterPlugin.java
@@ -10,11 +10,23 @@ import org.rundeck.storage.api.Path;
 import com.amazonaws.encryptionsdk.*;
 import com.amazonaws.encryptionsdk.kms.*;
 import java.util.Collections;
+import java.util.Map;
 import java.io.*;
+import com.dtolabs.utils.Streams;
 
 @Plugin(name="KmsConverterPlugin", service=ServiceNameConstants.StorageConverter)
 @PluginDescription(title="Rundeck KMS Plugin", description="Encrypts storage data using Amazon KMS")
 public class KmsConverterPlugin implements StorageConverterPlugin {
+  public static final String KMS_WAS_ENCRYPTED = "kms-converter-plugin:is-kms-encrypted";
+
+  static boolean wasEncrypted(ResourceMetaBuilder resourceMetaBuilder) {
+    return Boolean.parseBoolean(resourceMetaBuilder.getResourceMeta().get(KMS_WAS_ENCRYPTED));
+  }
+
+  static void addMetadataWasEncrypted(ResourceMetaBuilder resourceMetaBuilder) {
+    resourceMetaBuilder.setMeta(KMS_WAS_ENCRYPTED, Boolean.TRUE.toString());
+  }
+
   /**
    * The ARN to the Key Management Service Key to access.
    */
@@ -26,24 +38,14 @@ public class KmsConverterPlugin implements StorageConverterPlugin {
       Path path,
       ResourceMetaBuilder resourceMetaBuilder,
       HasInputStream hasInputStream){
-    var crypto = AwsCrypto.builder()
-      .withCommitmentPolicy(CommitmentPolicy.RequireEncryptRequireDecrypt)
-      .build();
+    if (! wasEncrypted(resourceMetaBuilder)) {
+      // This resource wasn't encrypted with this plugin
+      return null;
+    }
 
     var keyProvider = KmsMasterKeyProvider.builder().buildStrict(keyArn);
     var materialsManager = new DefaultCryptoMaterialsManager(keyProvider);
-
-    return new HasInputStream() {
-      @Override
-      public InputStream getInputStream() throws IOException {
-        return crypto.createDecryptingStream(materialsManager, hasInputStream.getInputStream());
-      }
-
-      @Override
-      public long writeContent(OutputStream outputStream) throws IOException {
-        return hasInputStream.writeContent(outputStream);
-      }
-    };
+    return new DecryptionStream(hasInputStream, materialsManager);
   }
 
   /** encrypt data to be stored if necessary */
@@ -51,25 +53,12 @@ public class KmsConverterPlugin implements StorageConverterPlugin {
       Path path,
       ResourceMetaBuilder resourceMetaBuilder,
       HasInputStream hasInputStream){
-    var crypto = AwsCrypto.builder()
-      .withCommitmentPolicy(CommitmentPolicy.RequireEncryptRequireDecrypt)
-      .build();
 
     var keyProvider = KmsMasterKeyProvider.builder().buildStrict(keyArn);
-    var encryptionContext = Collections.singletonMap("path", path.toString());
+    var encryptionContext = Collections.singletonMap("path", path.getPath());
     var materialsManager = new DefaultCryptoMaterialsManager(keyProvider);
-
-    return new HasInputStream() {
-      @Override
-      public InputStream getInputStream() throws IOException {
-        return crypto.createEncryptingStream(materialsManager, hasInputStream.getInputStream(), encryptionContext);
-      }
-
-      @Override
-      public long writeContent(OutputStream outputStream) throws IOException {
-        return hasInputStream.writeContent(outputStream);
-      }
-    };
+    addMetadataWasEncrypted(resourceMetaBuilder);
+    return new EncryptionStream(hasInputStream, materialsManager, encryptionContext);
   }
 
   /** encrypt data to be stored if necessary */
@@ -79,5 +68,61 @@ public class KmsConverterPlugin implements StorageConverterPlugin {
       HasInputStream hasInputStream){
     // Same implementation
     return createResource(path, resourceMetaBuilder, hasInputStream);
+  }
+
+  /**
+   * A KMS encryption stream.
+   */
+  private static class EncryptionStream implements HasInputStream {
+    private final HasInputStream hasInputStream;
+    private final CryptoMaterialsManager materialsManager;
+    private final Map<String,String> encryptionContext;
+
+    private EncryptionStream(HasInputStream hasInputStream, CryptoMaterialsManager materialsManager, Map<String,String> encryptionContext) {
+      this.hasInputStream = hasInputStream;
+      this.materialsManager = materialsManager;
+      this.encryptionContext = encryptionContext;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+      var crypto = AwsCrypto.builder()
+        .withCommitmentPolicy(CommitmentPolicy.RequireEncryptRequireDecrypt)
+        .build();
+      return crypto.createEncryptingStream(materialsManager, hasInputStream.getInputStream(), encryptionContext);
+    }
+
+    @Override
+    public long writeContent(OutputStream outputStream) throws IOException {
+      long bytes = Streams.copyStream(getInputStream(), outputStream);
+      return bytes;
+    }
+  }
+
+  /**
+   * A KMS decryption stream
+   */
+  private static class DecryptionStream implements HasInputStream {
+    private final HasInputStream hasInputStream;
+    private final CryptoMaterialsManager materialsManager;
+
+    private DecryptionStream(HasInputStream hasInputStream, CryptoMaterialsManager materialsManager) {
+      this.hasInputStream = hasInputStream;
+      this.materialsManager = materialsManager;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+      var crypto = AwsCrypto.builder()
+        .withCommitmentPolicy(CommitmentPolicy.RequireEncryptRequireDecrypt)
+        .build();
+      return crypto.createDecryptingStream(materialsManager, hasInputStream.getInputStream());
+    }
+
+    @Override
+    public long writeContent(OutputStream outputStream) throws IOException {
+      return Streams.copyStream(getInputStream(), outputStream);
+    }
+
   }
 }
