93ad5ff15ac0b02ca7f620fd5e100fe85b7b0cda
Matt Farmer <matt@frmr.me>
2021-02-07 22:40:59 -0500

    Helpers for testing in a local rundeck

--- test-in-rundeck.sh
+++ test-in-rundeck.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -e
+
+docker build -t rundeck-kms-test-image test-image
+
 docker run -it \
   -e AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY \
   -e AWS_REGION \
+  -e RUNDECK_STORAGE_CONVERTER_1_TYPE=rundeck-kms-plugin \
+  -e RUNDECK_STORAGE_CONVERTER_1_PATH=keys \
+  -e RUNDECK_STORAGE_CONVERTER_1_CONFIG_KEYARN=$KMS_KEY_ARN \
   -p 4440:4440 \
   -v $(pwd)/lib/build/libs/rundeck-kms-plugin.jar:/home/rundeck/libext/rundeck-kms-plugin.jar \
-  rundeck/rundeck:3.3.9
+  rundeck-kms-test-image
