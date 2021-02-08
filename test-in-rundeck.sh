166d1642da5d89078bd7aa56f7a1aa9ac85cd034
Matt Farmer <matt@frmr.me>
2021-02-07 22:06:35 -0500

    Alright, this works?

--- test-in-rundeck.sh
+++ test-in-rundeck.sh
@@ -5,5 +5,5 @@ docker run -it \
   -e AWS_SECRET_ACCESS_KEY \
   -e AWS_REGION \
   -p 4440:4440 \
-  -v $(pwd)/lib/build/libs/rundeck-kms-plugin-all.jar:/home/rundeck/libext/rundeck-kms-plugin-all.jar \
+  -v $(pwd)/lib/build/libs/rundeck-kms-plugin.jar:/home/rundeck/libext/rundeck-kms-plugin.jar \
   rundeck/rundeck:3.3.9
